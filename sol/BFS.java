a9c23f36afac369def0efbd36c44ae8a7607def2
MidianY <88814169+MidianY@users.noreply.github.com>
2022-03-17 17:29:00 -0400

    pp

--- sol/BFS.java
+++ sol/BFS.java
@@ -1,12 +1,7 @@
 package sol;
-
 import src.IBFS;
 import src.IGraph;
-
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class BFS<V, E> implements IBFS<V, E> {
     private HashMap<V, E> cityTransport;
@@ -15,46 +10,40 @@ public class BFS<V, E> implements IBFS<V, E> {
         this.cityTransport = new HashMap<>();
     }
 
-
     // TODO: implement the getPath method!
     @Override
     public List<E> getPath(IGraph<V, E> graph, V start, V end) {
+        List<E> path = new ArrayList<>();
         if(checkExist(graph, start, end)){
             V current = graph.getEdgeSource(this.cityTransport.get(end));
             while(!current.equals(start)){
-                this.cityTransport.get(current);
+                path.add(this.cityTransport.get(current));
             }
-
         }
-
-        //if the city is equal to the start
-        //while loop
-        //look for end node in the hashmap
-        //get previous node from that edge
-        //building it backwards until the node that you hit is the start node
-        //if(checkExist())
-        return null;
+        return path;
     }
 
     //call this helper in getPath and tells you whether a path exists, if true it exists if false throw error
     private boolean checkExist(IGraph<V, E> graph, V start, V end){
-        LinkedList<E> toCheck = new LinkedList<>(graph.getOutgoingEdges(start)); //contains outgoing edges from the start
+        Queue<E> toCheck = new LinkedList<>(graph.getOutgoingEdges(start)); //contains outgoing edges from the start
         HashSet<E> visited = new HashSet<>(graph.getOutgoingEdges(start)); //not sure
 
-        while (!toCheck.isEmpty()){
-            E checkingVertex = toCheck.removeFirst();
-            V targetVertex = graph.getEdgeTarget(checkingVertex);
-            if(visited.contains(start)){
+        while (!toCheck.isEmpty()) {
+            E checkingEdge = toCheck.poll(); //prev
+            V targetVertex = graph.getEdgeTarget(checkingEdge); //next
+
+            if (visited.contains(start)) {
                 continue;
             }
-            if(!this.cityTransport.containsKey(targetVertex)){
-                this.cityTransport.put(targetVertex, checkingVertex);
-            }
-            if(end.equals(targetVertex)){
-                return true;
-            }
-            else{
-               toCheck.addAll(graph.getOutgoingEdges(targetVertex));
+            for (E edges : graph.getOutgoingEdges(targetVertex)) {
+                if (!this.cityTransport.containsKey(targetVertex)) {
+                    this.cityTransport.put(targetVertex, edges);
+                }
+                if (end.equals(targetVertex)) {
+                    return true;
+                } else {
+                    toCheck.addAll(graph.getOutgoingEdges(targetVertex));
+                }
             }
         }
         return false;
