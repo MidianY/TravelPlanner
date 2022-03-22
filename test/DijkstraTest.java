aa1a4fba8bc0bd9d883c4092342c012300f13f2e
MidianY <88814169+MidianY@users.noreply.github.com>
2022-03-21 23:53:22 -0400

    pp

--- test/DijkstraTest.java
+++ test/DijkstraTest.java
@@ -3,12 +3,16 @@ package test;
 import org.junit.Test;
 import sol.Dijkstra;
 import sol.TravelController;
+import sol.TravelGraph;
+import src.City;
 import src.IDijkstra;
 import src.Transport;
+import src.TransportType;
 import test.simple.SimpleEdge;
 import test.simple.SimpleGraph;
 import test.simple.SimpleVertex;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Function;
 
@@ -34,25 +38,46 @@ public class DijkstraTest {
     private SimpleVertex c;
     private SimpleVertex d;
     private SimpleVertex e;
+
+    private City boston;
+    private City providence;
+    private City newYork;
+    private City washington;
+    private City chicago;
+    private City kentucky;
+    private City durham;
+    private TravelGraph travelGraph;
+    private Dijkstra<City, Transport> dijkstra;
     private TravelController travelController;
 
     /**
-     * Creates a simple graph.
-     * You'll find a similar method in each of the Test files.
-     * Normally, we'd like to use @Before, but because each test may require a different setup,
-     * we manually call the setup method at the top of the test.
-     * <p>
-     * TODO: create more setup methods!
+     * Constructor of the DijkstraTest class. It instantiates both the dijkstra and travelController as instance variables
+     * to be used throughout the tests
      */
-
-    private void setup(){
+    public DijkstraTest(){
         this.travelController = new TravelController();
+        this.dijkstra = new Dijkstra<>();
+    }
+
+    /**
+     * Creates a graph based on the information from the cities3 and transport3 files we created
+     */
+    private void graph1(){
         this.travelController.load("data/cities4.csv", "data/transport4.csv");
     }
 
+    /**
+     * Creates a graph basing on information from the cities4 and transport4 files we created
+     */
+    public void graph2(){
+        this.travelController.load("data/cities4.csv", "data/transport4.csv");
+    }
+
+    /**
+     * Creates a simple graph
+     */
     private void createSimpleGraph() {
         this.graph = new SimpleGraph();
-
         this.a = new SimpleVertex("a");
         this.b = new SimpleVertex("b");
         this.c = new SimpleVertex("c");
@@ -72,6 +97,46 @@ public class DijkstraTest {
         this.graph.addEdge(this.c, new SimpleEdge(2, this.c, this.d));
         this.graph.addEdge(this.d, new SimpleEdge(1, this.d, this.b));
         this.graph.addEdge(this.d, new SimpleEdge(5, this.e, this.d));
+    }
+
+    /**
+     * Creates a complex graph
+     */
+    private void complexGraph(){
+        this.travelGraph = new TravelGraph();
+        this.boston = new City("Boston");
+        this.providence = new City("Providence");
+        this.newYork = new City("newYork");
+        this.chicago = new City("Chicago");
+        this.washington = new City("Washington");
+        this.kentucky = new City("Kentucky");
+        this.durham = new City("Durham");
+
+        this.travelGraph.addVertex(this.boston);
+        this.travelGraph.addVertex(this.washington);
+        this.travelGraph.addVertex(this.providence);
+        this.travelGraph.addVertex(this.newYork);
+        this.travelGraph.addVertex(this.chicago);
+
+        this.travelGraph.addEdge(this.providence, new Transport(this.providence, this.washington,
+                TransportType.BUS, 5.0, 8.0));
+        this.travelGraph.addEdge(this.providence, new Transport(this.providence, this.chicago,
+                TransportType.TRAIN, 10.0, 2.0));
+        this.travelGraph.addEdge(this.washington, new Transport(this.washington, this.newYork,
+                TransportType.BUS, 6.0, 1.0));
+        this.travelGraph.addEdge(this.chicago, new Transport(this.chicago, this.boston,
+                TransportType.BUS, 40.0, 10.0));
+        this.travelGraph.addEdge(this.chicago, new Transport(this.chicago, this.newYork,
+                TransportType.BUS, 20.0, 6.0));
+        this.travelGraph.addEdge(this.newYork, new Transport(this.newYork, this.boston,
+                TransportType.BUS, 30.0, 6.0));
+        this.travelGraph.addEdge(this.kentucky, new Transport(this.kentucky, this.chicago,
+                TransportType.BUS, 15.0, 5.0));
+        this.travelGraph.addEdge(this.kentucky, new Transport(this.kentucky, this.durham,
+                TransportType.BUS, 30.0, 1.0));
+        this.travelGraph.addEdge(this.durham, new Transport(this.durham, this.providence,
+                TransportType.BUS, 15.0, 5.0));
+
     }
 
 
@@ -99,11 +164,49 @@ public class DijkstraTest {
      */
     @Test
     public void testDijkstra() {
-        this.setup();
+        this.graph1();
         //Providence -> Chicago -> Boston
         assertEquals(2, this.travelController.fastestRoute("Providence", "Boston").size());
 
         //Providence -> Washington -> Florida -> Boston
         assertEquals(3, this.travelController.cheapestRoute("Providence", "Boston").size());
     }
+
+    /**
+     * Upon using the function to calculate the edgeweight, this test considers price, the expected output is three as the cheapest route
+     * involves taking three stops
+     */
+    @Test
+    public void testDijkstraCheapestPath(){
+        //Providence -> Washington -> Florida -> Boston
+        this.complexGraph();
+        Function<Transport, Double> edgeWeightCalculation = e -> e.getPrice();
+        assertEquals(3, this.dijkstra.getShortestPath(this.travelGraph, this.providence, this.boston, edgeWeightCalculation).size());
+
+    }
+
+    /**
+     * Upon using the function to calculate the edgeweight, this test considers minutes, the expected output is 2 as the fastest route
+     * involves taking two stops
+     */
+    @Test
+    public void testDijkstraFastestPath(){
+        //Providence -> Chicago -> Boston
+        this.complexGraph();
+        Function<Transport, Double> edgeWeightCalculation = e -> e.getMinutes();
+        assertEquals(2, this.dijkstra.getShortestPath(this.travelGraph, this.providence, this.boston, edgeWeightCalculation).size());
+    }
+
+    /**
+     * Ensures the algorithm returns an empty list when two cities have no path for the complex graph
+     */
+    @Test
+    public void testDijkstraNoPath(){
+        this.complexGraph();
+        List<Transport> noPath = new ArrayList<>();
+        Function<Transport, Double> edgeWeightCalculation = e -> e.getMinutes();
+        assertEquals(noPath, this.dijkstra.getShortestPath(this.travelGraph, this.boston, this.providence, edgeWeightCalculation));
+        assertEquals(0, this.dijkstra.getShortestPath(this.travelGraph, this.boston, this.providence, edgeWeightCalculation).size());
+    }
+
 }
